596a34611f8a13910dfce1a0a55c3f09f5c8eac8
MateuszGo <matgolosz@gmail.com>
2018-01-29 20:15:41 +0100

    small improvements

--- chess.py
+++ chess.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
 
-# Author: Shao Zhang and Phil Saltzman
-# Models: Eddie Canaan
-# Last Updated: 2015-03-13
-#
-# This tutorial shows how to determine what objects the mouse is pointing to
-# We do this using a collision ray that extends from the mouse position
-# and points straight into the scene, and see what it collides with. We pick
-# the object with the closest collision
 
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import CollisionTraverser, CollisionNode
@@ -21,7 +12,6 @@ from direct.task.Task import Task
 from enum import Enum
 import sys
 
-# First we define some constants for the colors
 BLACK = (0, 0, 0, 1)
 WHITE = (1, 1, 1, 1)
 HIGHLIGHT_POSITIVE = (0.498039, 1, 0, 1)
@@ -30,26 +20,15 @@ PIECEBLACK = (.15, .15, .15, 1)
 
 check = None
 
-# Now we define some helper functions that we will need later
-
-# This function, given a line (vector plus origin point) and a desired z value,
-# will give us the point on the line where the desired z value is what we want.
-# This is how we know where to position an object in 3D space based on a 2D mouse
-# position. It also assumes that we are dragging in the XY plane.
-#
-# This is derived from the mathematical of a plane, solved for a given point
 def PointAtZ(z, point, vec):
     return point + vec * ((z - point.getZ()) / vec.getZ())
 
-# A handy little function for getting the proper position for a given square1
 def SquarePos(i):
     return LPoint3((i % 8) - 3.5, int(i // 8) - 3.5, 0)
 
 def trash():
     return LPoint3(-500, -500)
 
-# Helper function for determining whether a square should be white or black
-# The modulo operations (%) generate the every-other pattern of a chess-board
 def SquareColor(i):
     if (i + ((i // 8) % 2)) % 2:
         return BLACK
@@ -78,8 +57,6 @@ class ChessboardDemo(ShowBase):
             if (not isinstance(self.pieces[possibleEndPosition], type(None))):
                 if (self.pieces[possibleEndPosition].obj.getColor() == self.turn):
                     collisions.append(possibleEndPosition)
-               # else:
-                    #collisions.append(possibleEndPosition)
         return collisions
 
     def getDirectionMultiplier(self, startingPos, endPos):
@@ -148,14 +125,11 @@ class ChessboardDemo(ShowBase):
 
 
     def __init__(self):
-        # Initialize the ShowBase class from which we inherit, which will
-        # create a window and set up everything we need for rendering into it.
         ShowBase.__init__(self)
         self.turn = WHITE
         self.movingPiece = None
         self.isMovementPossible = False
 
-        # This code puts the standard title and instruction text on screen
         self.escapeEvent = OnscreenText(
             text="ESC: Quit", parent=base.a2dTopLeft,
             style=1, fg=(1, 1, 1, 1), pos=(0.06, -0.1),
@@ -167,33 +141,20 @@ class ChessboardDemo(ShowBase):
 
         self.accept('escape', sys.exit)  # Escape quits
         self.disableMouse()  # Disble mouse camera control
-        camera.setPosHpr(0, -12, 8, 0, -35, 0)  # Set the camera
-        self.setupLights()  # Setup default lighting
+        camera.setPosHpr(0, -12, 8, 0, -35, 0)
+        self.setupLights()
 
-        # Since we are using collision detection to do picking, we set it up like
-        # any other collision detection system with a traverser and a handler
         self.picker = CollisionTraverser()  # Make a traverser
         self.pq = CollisionHandlerQueue()  # Make a handler
-        # Make a collision node for our picker ray
         self.pickerNode = CollisionNode('mouseRay')
-        # Attach that node to the camera since the ray will need to be positioned
-        # relative to it
         self.pickerNP = camera.attachNewNode(self.pickerNode)
-        # Everything to be picked will use bit 1. This way if we were doing other
-        # collision we could separate it
         self.pickerNode.setFromCollideMask(BitMask32.bit(1))
         self.pickerRay = CollisionRay()  # Make our ray
         # Add it to the collision node
         self.pickerNode.addSolid(self.pickerRay)
         # Register the ray as something that can cause collisions
         self.picker.addCollider(self.pickerNP, self.pq)
-        # self.picker.showCollisions(render)
 
-        # Now we create the chess board and its pieces
-
-        # We will attach all of the squares to their own root. This way we can do the
-        # collision pass just on the squares and save the time of checking the rest
-        # of the scene
         self.squareRoot = render.attachNewNode("squareRoot")
         # For each square
         self.squares = [None for i in range(64)]
@@ -205,22 +166,11 @@ class ChessboardDemo(ShowBase):
             self.squares[i].reparentTo(self.squareRoot)
             self.squares[i].setPos(SquarePos(i))
             self.squares[i].setColor(SquareColor(i))
-            # Set the model itself to be collideable with the ray. If this model was
-            # any more complex than a single polygon, you should set up a collision
-            # sphere around it instead. But for single polygons this works
-            # fine.
+
             self.squares[i].find("**/polygon").node().setIntoCollideMask(
                 BitMask32.bit(1))
-            # Set a tag on the square's node so we can look up what square this is
-            # later during the collision pass
             self.squares[i].find("**/polygon").node().setTag('square', str(i))
 
-            # We will use this variable as a pointer to whatever piece is currently
-            # in this square
-
-        # The order of pieces on a chessboard from white's perspective. This list
-        # contains the constructor functions for the piece classes defined
-        # below
         pieceOrder = (Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook)
 
         for i in range(8, 16):
@@ -263,15 +213,11 @@ class ChessboardDemo(ShowBase):
         self.pieces[index] = None
 
     def mouseTask(self, task):
-        # This task deals with the highlighting and dragging based on the mouse
 
-        # First, clear the current highlight
         if self.hiSq is not False:
             self.squares[self.hiSq].setColor(SquareColor(self.hiSq))
             self.hiSq = False
 
-        # Check to see if we can access the mouse. We need it to do anything
-        # else
         if self.mouseWatcherNode.hasMouse():
             # get the mouse position
             mpos = self.mouseWatcherNode.getMouse()
@@ -292,12 +238,8 @@ class ChessboardDemo(ShowBase):
                 self.pieces[self.dragging].obj.setPos(
                     PointAtZ(.5, nearPoint, nearVec))
 
-            # Do the actual collision pass (Do it only on the squares for
-            # efficiency purposes)
             self.picker.traverse(self.squareRoot)
             if self.pq.getNumEntries() > 0:
-                # if we have hit something, sort the hits so that the closest
-                # is first, and highlight that node
                 self.pq.sortEntries()
                 i = int(self.pq.getEntry(0).getIntoNode().getTag('square'))
                 # Set the highlight on the picked square
@@ -313,15 +255,12 @@ class ChessboardDemo(ShowBase):
                             self.isMovementPossible = True
                         else:
                             self.squares[i].setColor(HIGHLIGHT_NEGATIVE)
-                            #self.pieces[i] = None
                             self.isMovementPossible = False
                     self.hiSq = i
 
         return Task.cont
 
     def grabPiece(self):
-        # If a square is highlighted and it has a piece, set it to dragging
-        # mode
         if self.hiSq is not False and self.pieces[self.hiSq]:
             self.movingPiece = self.pieces[self.hiSq]
             self.movingPieceIndex = self.hiSq
@@ -329,11 +268,7 @@ class ChessboardDemo(ShowBase):
             self.hiSq = False
 
     def releasePiece(self):
-        # Letting go of a piece. If we are not on a square, return it to its original
-        # position. Otherwise, swap it with the piece in the new square
-        # Make sure we really are dragging something
         if self.dragging is not False:
-            # We have let go of the piece, but we are not on a square
             if self.hiSq is False or not self.isMovementPossible or (isinstance(self.pieces[self.dragging], Piece) and isinstance(self.pieces[self.hiSq], Piece) and self.pieces[self.dragging].obj.getColor() == self.pieces[self.hiSq].obj.getColor()):
                 self.pieces[self.dragging].obj.setPos(
                     SquarePos(self.dragging))
@@ -349,7 +284,6 @@ class ChessboardDemo(ShowBase):
                 self.isCheck(self.hiSq)
                 self.changeTurn()
 
-        # We are no longer dragging anything
         self.dragging = False
         self.movingPiece = None
         self.movingPieceIndex = -1
@@ -393,9 +327,6 @@ class ChessboardDemo(ShowBase):
                     
 
 
-
-# Class for a piece. This just handles loading the model and setting initial
-# position and color
 class Piece(object):
     def __init__(self, square, color):
         self.obj = loader.loadModel(self.model)
@@ -413,12 +344,6 @@ class Piece(object):
         return False
 
 
-
-# Classes for each type of chess piece
-# Obviously, we could have done this by just passing a string to Piece's init.
-# But if you wanted to make rules for how the pieces move, a good place to start
-# would be to make an isValidMove(toSquare) method for each piece type
-# and then check if the destination square is acceptible during ReleasePiece
 class Pawn(Piece):
     model = "models/pawn"
 
@@ -481,6 +406,5 @@ class Rook(Piece):
     def getMovementTableBattle(self):
         return [[0, 8, 0], [8, 0, 8], [0, 8, 0]]
 
-# Do the main initialization and start 3D rendering
 demo = ChessboardDemo()
 demo.run()
